8b83c59bff504f19192edee4e917879c536839ab
Pdzly <34943569+Pdzly@users.noreply.github.com>
2023-06-18 15:43:53 +0200

    chore: Moved to discordX

---
+++ src/commands/ping.ts
@@ -0,0 +1,13 @@
+import type { CommandInteraction } from "discord.js";
+import { Discord, Slash } from "discordx";
+
+@Discord()
+export class PingCommand {
+  // example: pagination for all slash command
+  @Slash({
+    description: "Ping-Pong!",
+  })
+  async ping(interaction: CommandInteraction): Promise<void> {
+    interaction.reply("Pong!")
+  }
+}
--- src/models/IEvent.ts
+++
@@ -1,7 +0,0 @@
-import { ClientEvents } from "discord.js";
-
-export default interface IEvent {
-    name: keyof ClientEvents
-    once?: boolean
-    execute: (...args:any) => Promise<void> | void
-}
