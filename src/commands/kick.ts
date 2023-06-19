299e2c7d2721a8b32039bfaf47137c5df96673ad
Jeymen <jeymen11@outlook.com>
2023-06-19 21:30:15 +0300

    feat: Add kick command

---
+++ src/commands/kick.ts
@@ -0,0 +1,47 @@
+import { ApplicationCommandOptionType, CommandInteraction, Guild, GuildMember } from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+
+@Discord()
+export class KickCommand {
+    @Slash({
+        description: "Kicks a user",
+        dmPermission: false,
+    })
+    async kick(
+        @SlashOption({
+            name: "user",
+            description: "The user to kick",
+            required: true,
+            type: ApplicationCommandOptionType.User,
+
+        })
+        @SlashOption({
+            name: "reason",
+            description: "Reason for the kick",
+            required: true,
+            type: ApplicationCommandOptionType.String,
+        })
+        user: GuildMember,
+        reason: string,
+        interaction: CommandInteraction
+    ): Promise<void> {
+        const guild: Guild | null = interaction.guild;
+        await interaction.deferReply();
+
+        if (guild && user) {
+            if (user === interaction.member as GuildMember) {
+                await interaction.editReply("⛔ You can't kick yourself.")
+                return
+            }
+            try {
+                await guild.members.kick(user, reason);
+                await interaction.editReply("✅ User has been kicked.");
+            } catch (error) {
+                console.error("Error kicking user:", error);
+                await interaction.editReply("⛔ Failed to kick user.");
+            }
+        } else {
+            await interaction.editReply("⛔ Failed to kick user.");
+        }
+    }
+}
