f6512f000a7d234c0cd52fcc103daf21962c1460
Pdzly <34943569+Pdzly@users.noreply.github.com>
2023-06-19 21:01:40 +0200

    feat: Collected Ban and Kick to 1 File and patched ban too

---
+++ src/commands/userManagement.ts
@@ -0,0 +1,94 @@
+import {
+  ApplicationCommandOptionType,
+  CommandInteraction,
+  Guild,
+  GuildMember,
+} from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+
+@Discord()
+export class BanCommand {
+  @Slash({
+    description: "Kicks a user",
+    dmPermission: false,
+    defaultMemberPermissions: ["KickMembers"],
+  })
+  async kick(
+    @SlashOption({
+      name: "user",
+      description: "The user to kick",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    })
+    user: GuildMember,
+    @SlashOption({
+      name: "reason",
+      description: "Reason for the kick",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    reason: string,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const guild: Guild | null = interaction.guild;
+    await interaction.deferReply();
+
+    if (guild && user) {
+      if (user === (interaction.member as GuildMember)) {
+        await interaction.editReply("⛔ You can't kick yourself.");
+        return;
+      }
+      try {
+        await guild.members.kick(user, reason);
+        await interaction.editReply("✅ User has been kicked.");
+      } catch (error) {
+        console.error("Error kicking user:", error);
+        await interaction.editReply("⛔ Failed to kick user.");
+      }
+    } else {
+      await interaction.editReply("⛔ Failed to kick user.");
+    }
+  }
+
+  @Slash({
+    description: "Bans a user",
+    dmPermission: false,
+    defaultMemberPermissions: ["BanMembers"],
+  })
+  async ban(
+    @SlashOption({
+      name: "user",
+      description: "The user to ban",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    })
+    user: GuildMember,
+    @SlashOption({
+      name: "reason",
+      description: "Reason for the ban",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    reason: string,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const guild: Guild | null = interaction.guild;
+    await interaction.deferReply();
+
+    if (guild && user) {
+      if (user === (interaction.member as GuildMember)) {
+        await interaction.editReply("⛔ You can't ban yourself");
+        return;
+      }
+      try {
+        await guild.members.ban(user, { reason: reason });
+        await interaction.editReply("✅ User has been banned.");
+      } catch (error) {
+        console.error("Error banning user:", error);
+        await interaction.editReply("⛔ Failed to ban user.");
+      }
+    } else {
+      await interaction.editReply("⛔ Failed to ban user.");
+    }
+  }
+}
