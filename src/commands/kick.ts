2f369ef441d75d4a8c716e785f4165fc135ee2c8
Pdzly <34943569+Pdzly@users.noreply.github.com>
2023-06-19 21:21:09 +0200

    broke ban and kick again

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -7,7 +7,7 @@ import {
 import { Discord, Slash, SlashOption } from "discordx";
 
 @Discord()
-export class UserManagement {
+export class kick {
   @Slash({
     description: "Kicks a user",
     dmPermission: false,
@@ -30,7 +30,13 @@ export class UserManagement {
     reason: string,
     interaction: CommandInteraction
   ): Promise<void> {
-    const guild: Guild | null = interaction.guild;
+
+    if(!interaction.guild){
+      await interaction.editReply("⛔ You must be in a guild!");
+      return
+    }
+
+    const guild: Guild = interaction.guild;
     await interaction.deferReply();
 
     if (guild && user) {
@@ -47,48 +53,6 @@ export class UserManagement {
       }
     } else {
       await interaction.editReply("⛔ Failed to kick user.");
-    }
-  }
-
-  @Slash({
-    description: "Bans a user",
-    dmPermission: false,
-    defaultMemberPermissions: ["BanMembers"],
-  })
-  async ban(
-    @SlashOption({
-      name: "user",
-      description: "The user to ban",
-      required: true,
-      type: ApplicationCommandOptionType.User,
-    })
-    user: GuildMember,
-    @SlashOption({
-      name: "reason",
-      description: "Reason for the ban",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-    })
-    reason: string,
-    interaction: CommandInteraction
-  ): Promise<void> {
-    const guild: Guild | null = interaction.guild;
-    await interaction.deferReply();
-
-    if (guild && user) {
-      if (user === (interaction.member as GuildMember)) {
-        await interaction.editReply("⛔ You can't ban yourself");
-        return;
-      }
-      try {
-        await guild.members.ban(user, { reason: reason });
-        await interaction.editReply("✅ User has been banned.");
-      } catch (error) {
-        console.error("Error banning user:", error);
-        await interaction.editReply("⛔ Failed to ban user.");
-      }
-    } else {
-      await interaction.editReply("⛔ Failed to ban user.");
     }
   }
 }
