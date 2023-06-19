3aecbe064c58be6cead8d1f73373af4f716e8cde
Jeymen <jeymen11@outlook.com>
2023-06-19 18:00:53 +0300

    feat: Improve styling of Discord embed

--- src/commands/userinfo.ts
+++ src/commands/userinfo.ts
@@ -1,70 +1,66 @@
 import Dangercord from "dangercord";
 import {
-  ApplicationCommandOptionType,
-  CommandInteraction,
-  EmbedBuilder,
-  GuildMember,
-  User,
+    ApplicationCommandOptionType,
+    CommandInteraction,
+    EmbedBuilder,
+    GuildMember
 } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 
 @Discord()
 export class UserInfoCommand {
-  dangercord: Dangercord;
-  constructor() {
-    if (!process.env.DANGERCORD_TOKEN)
-      throw new Error("DANGERCORD_TOKEN is not defined!");
-    this.dangercord = new Dangercord(process.env.DANGERCORD_TOKEN);
-  }
-  @Slash({
-    description: "Displays information about the user",
-    dmPermission: false,
-  })
-  async userinfo(
-    @SlashOption({
-      name: "user",
-      description: "The user to get information about",
-      required: false,
-      type: ApplicationCommandOptionType.User,
-    })
-    user: GuildMember | undefined,
-    interaction: CommandInteraction
-  ): Promise<void> {
-    await interaction.deferReply();
-    if (!user) {
-      user = interaction.member as GuildMember;
+    dangercord: Dangercord;
+    constructor() {
+        if (!process.env.DANGERCORD_TOKEN)
+            throw new Error("DANGERCORD_TOKEN is not defined!");
+        this.dangercord = new Dangercord(process.env.DANGERCORD_TOKEN);
     }
-    var result = await this.dangercord.getUser(user.id);
-
-    const userInfo = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("User Information")
-      .setAuthor({
-        name: "The Coding Empire",
-        iconURL:
-          "https://cdn.discordapp.com/icons/952138215136055329/a_9ada81bcdd8f523936199b56106bd222.webp?size=96",
-        url: "https://discord.gg/Z7NDVrTmDN",
-      })
-      .setColor("#d3535f")
-      .addFields(
-        { name: "User ID:", value: `${user.id}` },
-        { name: "Username:", value: `${user.displayName}` },
-        { name: "Created at:", value: `${user.user.createdAt}` },
-        { name: "Is a bot?:", value: `${user.user.bot == true}` },
-        { name: "Dangercord:", value: `-----------` },
-        { name: "Reports:", value: `${result.reports}` },
-        { name: "Spammer:", value: `${result.flags?.spammer == true}`, inline: true  },
-        { name: "Blacklisted:", value: `${result.badges.blacklisted == true}`, inline: true  },
-        { name: "Raid Bot:", value: `${result.badges.raid_bot == true}`, inline: true  },
-        { name: "Scam Bot:", value: `${result.badges.scam_bot == true}`, inline: true },
-        {
-          name: "Votes:",
-          value: `👍:${result.votes.upvotes}
----------------------------------
-👎: ${result.votes.downvotes}`,
+    @Slash({
+        description: "Displays information about the user",
+        dmPermission: false,
+    })
+    async userinfo(
+        @SlashOption({
+            name: "user",
+            description: "The user to get information about",
+            required: false,
+            type: ApplicationCommandOptionType.User,
+        })
+        user: GuildMember | undefined,
+        interaction: CommandInteraction
+    ): Promise<void> {
+        await interaction.deferReply();
+        if (!user) {
+            user = interaction.member as GuildMember;
         }
-      )
-      .setTimestamp();
-    interaction.editReply({ embeds: [userInfo] });
-  }
+        var result = await this.dangercord.getUser(user.id);
+
+        const userInfo = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("User Information")
+            .setAuthor({
+                name: "The Coding Empire",
+                iconURL:
+                    "https://cdn.discordapp.com/icons/952138215136055329/a_9ada81bcdd8f523936199b56106bd222.webp?size=96",
+                url: "https://discord.gg/Z7NDVrTmDN",
+            })
+            .setColor("#d3535f")
+            .addFields(
+                { name: "**User ID**:", value: `${user.id}` },
+                { name: "**Username**:", value: `${user.displayName}` },
+                { name: "**Created at**:", value: `${user.user.createdAt}` },
+                { name: "**Is a bot?**:", value: `${user.user.bot == true}` },
+                { name: "**Reports**:", value: `${result.reports}` },
+                { name: "**Spammer**:", value: `${result.flags?.spammer == true}` },
+                { name: "**Blacklisted**:", value: `${result.badges.blacklisted == true}` },
+                { name: "**Raid Bot**:", value: `${result.badges.raid_bot == true}` },
+                { name: "**Scam Bot**:", value: `${result.badges.scam_bot == true}` },
+                {
+                    name: "Votes:",
+                    value: `👍:${result.votes.upvotes} \n👎: ${result.votes.downvotes}`,
+                }
+            )
+            .setTimestamp();
+        interaction.editReply({ embeds: [userInfo] });
+    }
 }
