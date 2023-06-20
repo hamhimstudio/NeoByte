5814b55c3bf5496743ccf202c04172dd284b7939
Jeymen <jeymen11@outlook.com>
2023-06-20 21:11:28 +0300

    feat: Add unmute command

--- src/commands/mute.ts
+++ src/commands/mute.ts
@@ -4,26 +4,26 @@ import { Discord, Slash, SlashOption } from "discordx";
 @Discord()
 export class MuteCommand {
     @Slash({
-        description: "Timeouts a user",
+        description: "Mute a user",
         dmPermission: false,
-        defaultMemberPermissions: ["MuteMembers"],
+        defaultMemberPermissions: ["KickMembers"],
     })
     async mute(
         @SlashOption({
             name: "user",
-            description: "The user to timeout",
+            description: "The user to mute",
             required: true,
             type: ApplicationCommandOptionType.User,
         }) user: GuildMember,
         @SlashOption({
             name: "reason",
-            description: "Reason for the timeout",
+            description: "Reason for the mute",
             required: true,
             type: ApplicationCommandOptionType.String,
         }) reason: string,
         @SlashOption({
             name: "time",
-            description: "Time for timeout in minutes.",
+            description: "Time for mute in minutes.",
             required: true,
             type: ApplicationCommandOptionType.Integer,
         }) time: number,
@@ -46,17 +46,51 @@ export class MuteCommand {
         }
 
         if (user === interaction.member) {
-            await interaction.editReply("You cannot timeout yourself!");
+            await interaction.editReply("You cannot mute yourself!");
             return;
         }
 
         try {
             await user.timeout(timeoutTime);
-            await interaction.editReply(`${user} has been timed out for ${time} minute(s) for ${reason}.`);
+            await interaction.editReply(`${user} has been muted for ${time} minute(s) for ${reason}.`);
 
         } catch (error) {
-            console.error("Error timing out user:", error);
-            await interaction.editReply("Failed to timeout user!");
+            await interaction.editReply("Failed to mute user!");
+        }
+    }
+
+
+    @Slash({
+        description: "Unmute a member",
+        dmPermission: false,
+        defaultMemberPermissions: ["MuteMembers"],
+    })
+    async unmute(
+        @SlashOption({
+            name: "user",
+            description: "The user to unmute",
+            required: true,
+            type: ApplicationCommandOptionType.User,
+        }) user: GuildMember,
+        interaction: CommandInteraction
+    ): Promise<void> {
+
+        if (!interaction.guild) {
+            await interaction.editReply("You must be in a guild!");
+            return;
+        }
+
+        if (user === interaction.member) {
+            await interaction.editReply("You cannot unmute yourself!");
+            return;
+        }
+
+        try {
+            await user.timeout(null);
+            await interaction.editReply(`${user} has been unmuted.`);
+
+        } catch (error) {
+            await interaction.editReply("Failed to unmute user!");
         }
     }
 }
