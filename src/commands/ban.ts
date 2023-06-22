ed25bbe5246826de4c64ff9521a59f52e3708b6c
Jeymen <109573368+Jeymen@users.noreply.github.com>
2023-06-22 20:41:13 +0300

    feat: Improve ban command

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -1,13 +1,8 @@
-import {
-  ApplicationCommandOptionType,
-  CommandInteraction,
-  Guild,
-  GuildMember,
-} from "discord.js";
+import { ApplicationCommandOptionType, CommandInteraction, Guild, GuildMember } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 
 @Discord()
-export class ban {
+export class BanCommand {
   @Slash({
     description: "Bans a user",
     dmPermission: false,
@@ -30,34 +25,35 @@ export class ban {
     reason: string,
     interaction: CommandInteraction
   ): Promise<void> {
-
     if (!interaction.guild) {
-      await interaction.editReply("You must be in a guild!");
-      return
+      await interaction.reply("You must be in a guild!");
+      return;
     }
 
     const guild: Guild = interaction.guild;
-    await interaction.deferReply();
 
-    if (guild && user) {
-      if (user === (interaction.member as GuildMember)) {
-        await interaction.editReply("You can't ban yourself");
-        return;
-      }
-      try {
-        await user.send(`You have been banned for ${reason} in ${interaction.guild.name}.`)
-          .catch(() => interaction.followUp(`Can't send DM to ${user}!`));
-      } catch (error) {
-        console.error("Error banning user:", error);
-        await interaction.editReply(`Failed to ban ${user}!`);
-      }
+    if (user === interaction.member) {
+      await interaction.reply("You can't ban yourself");
+      return;
+    }
+
+    try {
+      await user.send(`You have been banned for ${reason} in ${interaction.guild.name}.`);
       await guild.members.ban(user, { reason: reason });
-      await interaction.editReply(`${user} has been banned for ${reason}.`);
-
-    } else {
-      await interaction.editReply(`Failed to ban ${user}!`);
+      await interaction.reply(`${user} has been banned for ${reason}.`);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message === "Cannot send messages to this user") {
+          await guild.members.ban(user, { reason: reason });
+          await interaction.reply(`${user} has been banned for ${reason}.\nNote: Can't DM user.`);
+        } else {
+          console.error("Error banning user:", error);
+          await interaction.reply(`Failed to ban ${user}! Error: ${error.message}`);
+        }
+      }
     }
   }
+
   @Slash({
     description: "Unban a user",
     dmPermission: false,
@@ -73,32 +69,27 @@ export class ban {
     user: GuildMember,
     interaction: CommandInteraction
   ): Promise<void> {
-
-    await interaction.deferReply();
-
     if (!interaction.guild) {
-      await interaction.editReply("You must be in a guild!");
+      await interaction.reply("You must be in a guild!");
       return;
     }
 
-    const guild: Guild | null = interaction.guild;
-
-    if (user === interaction.member) {
-      await interaction.editReply("You cannot ban yourself!");
-      return;
-    }
+    const guild: Guild = interaction.guild;
 
     try {
-      await user.send(`You have been unbanned in ${interaction.guild.name}.`)
-        .catch(() => interaction.followUp(`Can't send DM to ${user}!`));
-
+      await user.send(`You have been unbanned from ${interaction.guild.name}.`);
+      await guild.members.unban(user);
+      await interaction.reply(`${user} has been unbanned.`);
     } catch (error) {
-      console.error("Error unbanning user:", error);
-      await interaction.editReply(`Failed to unban ${user}!`);
-
+      if (error instanceof Error) {
+        if (error.message === "Cannot send messages to this user") {
+          await guild.members.unban(user);
+          await interaction.reply(`${user} has been unbanned.\nNote: Can't DM user.`);
+        } else {
+          console.error("Error unbanning user:", error);
+          await interaction.reply(`Failed to unban ${user}! Error: ${error.message}`);
+        }
+      }
     }
-    await guild.members.unban(user);
-    await interaction.editReply(`${user} has been unbanned.`);
-
   }
 }
