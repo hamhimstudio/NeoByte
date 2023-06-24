d68a6179836824c1bd177e10d15d8914907c32a2
Jeymen <jeymen11@outlook.com>
2023-06-24 20:38:56 +0300

    feat: Update DB settings

--- src/repositories/connection.ts
+++ src/repositories/connection.ts
@@ -5,8 +5,8 @@ import warn from "../models/warn.js";
 const connection = new DataSource({
     type: "mongodb",
     host: process.env.MONGODB_URL || "localhost",
-    port: 2020,
-    database: process.env.MONGODB_DB || "tce-bot",
+    port: Number(process.env.MONGODB_PORT)|| 27017,
+    database: process.env.MONGODB_DB || "discord-bot",
     username: process.env.MONGODB_USERNAME,
     password: process.env.MONGODB_PASSWORD,
     entities: [warn],
