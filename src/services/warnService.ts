3ee3c0cd6aba56e2c3fcfe8603294deb7740f275
Jeymen <jeymen11@proton.me>
2023-07-20 21:02:56 +0300

    feat: Enchance warn points system

--- src/services/warnService.ts
+++ src/services/warnService.ts
@@ -1,8 +1,7 @@
-import { ObjectId } from "mongodb";
-import { Inject, Service } from "typedi";
-import warn from "../models/warn.js";
-import warnRepository from "../repositories/warnRepository.js";
-
+import { ObjectId } from 'mongodb'
+import { Inject, Service } from 'typedi'
+import warn from '../models/warn.js'
+import warnRepository from '../repositories/warnRepository.js'
 
 @Service()
 export default class warnService {
@@ -10,23 +9,45 @@ export default class warnService {
     warnRepository: warnRepository
 
     async findWarning(id: string) {
-        return await this.warnRepository.findOne({ where: { _id: ObjectId.createFromHexString(id) 
-        } })
+        return await this.warnRepository.findOne({
+            where: { _id: ObjectId.createFromHexString(id) },
+        })
     }
 
     async getWarningForUser(userId: string, guildId: string) {
-        return await this.warnRepository.getWarningForUser(userId, guildId);
+        return await this.warnRepository.getWarningForUser(userId, guildId)
     }
 
-    async createWarning(userId: string, guildId: string, reason: string, moderatorUserId: string, anonymous: boolean, points: number) {
-        return await this.warnRepository.createWarning(userId, guildId, reason, moderatorUserId, anonymous, points);
-      }
-      
+    async createWarning(
+        userId: string,
+        guildId: string,
+        reason: string,
+        moderatorUserId: string,
+        anonymous: boolean,
+        points: number
+    ) {
+        return await this.warnRepository.createWarning(
+            userId,
+            guildId,
+            reason,
+            moderatorUserId,
+            anonymous,
+            points
+        )
+    }
+
     async removeWarning(warn: warn) {
-        return await this.warnRepository.removeWarning(warn);
+        return await this.warnRepository.removeWarning(warn)
     }
     async updateWarning(warn: warn) {
-        return await this.warnRepository.save(warn);
+        return await this.warnRepository.save(warn)
     }
-
+    async getTotalPoints( userId: string, guildId: string) {
+        let totalPoints = 0
+        const warnings = await this.getWarningForUser(userId, guildId)
+        for (const warning of warnings) {
+            totalPoints += warning.points
+        }
+        return totalPoints
+    }
 }
